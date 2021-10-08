63e00112798a6a5dddfbc908542b4d53c644c2fe
Mai Tien Hai <haimt7@viettel.com.vn>
2021-10-08 10:52:41 +0700

    readme for curl add to cart

--- rest/README.md
+++ rest/README.md
@@ -9,3 +9,13 @@ curl -X GET \
     "http://localhost:8080/api/v1/products?category-id=1&brand-id=1&color-id=1&price-min=0&price-max=5100000"
 ```
 
+```bash
+curl -X POST \
+    -H "Content-type: application/json" \
+    -H "Accept: application/json" \
+    -d '{"productId": 18, "quantity": 2}' \
+    "http://localhost:8080/api/v1/carts"
+```
+
+mvn clean package 
+mvn clean install
---
+++ rest/src/main/java/com/maitienhai/icommerce/InsertTestDataRunner.java
@@ -0,0 +1,57 @@
+package com.maitienhai.icommerce;
+
+import com.maitienhai.icommerce.domain.Product;
+import com.maitienhai.icommerce.domain.ProductBrand;
+import com.maitienhai.icommerce.domain.ProductCategory;
+import com.maitienhai.icommerce.domain.ProductColor;
+import com.maitienhai.icommerce.repository.ProductBrandRepository;
+import com.maitienhai.icommerce.repository.ProductCategoryRepository;
+import com.maitienhai.icommerce.repository.ProductColorRepository;
+import com.maitienhai.icommerce.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.Collections;
+
+@Profile("test")
+@Component
+@RequiredArgsConstructor
+public class InsertTestDataRunner implements CommandLineRunner {
+
+    private final ProductRepository productRepository;
+
+    private final ProductCategoryRepository productCategoryRepository;
+
+    private final ProductBrandRepository productBrandRepository;
+
+    private final ProductColorRepository productColorRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        if (productRepository.count() == 0) {
+            ProductCategory category = ProductCategory.builder().name("T-shirt").build();
+            ProductBrand brand = ProductBrand.builder().name("Adidas").build();
+            ProductColor color = ProductColor.builder().name("Red").build();
+
+            Product product = Product.builder().name("Ultraboost")
+                    .brand(brand)
+                    .category(category)
+                    .color(color)
+                    .price(BigDecimal.valueOf(5000000))
+                    .build();
+            category.setProducts(Collections.singletonList(product));
+            brand.setProducts(Collections.singletonList(product));
+            color.setProducts(Collections.singletonList(product));
+            productRepository.save(product);
+
+            productCategoryRepository.save(ProductCategory.builder().name("Sneaker").build());
+
+            productBrandRepository.save(ProductBrand.builder().name("Nike").build());
+
+            productColorRepository.save(ProductColor.builder().name("Blue").build());
+        }
+    }
+}
